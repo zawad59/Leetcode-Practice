a491f967bb261fa7c2842b2851c2f452d0c140d0
zawad59 <tawsikzawad@gmail.com>
2024-05-19 00:58:34 -0400

    LinkedListCycle

---
+++ LinkedListCycle/Solution.py
@@ -0,0 +1,10 @@
+
+class Solution(object):
+    def hasCycle(self, head):
+        slow, fast = head, head
+        while(fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+            if(slow==fast):
+                return True
+        return False
