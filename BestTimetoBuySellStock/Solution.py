e0ed662b4566a8663f8bf0cfd63dc55759077194
zawad59 <tawsikzawad@gmail.com>
2025-12-23 21:24:44 +0600

    Fix maxProfit calculation in stock price analysis
    Refactor maxProfit calculation to improve logic.

--- BestTimetoBuySellStock/Solution.py
+++ BestTimetoBuySellStock/Solution.py
@@ -1,14 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
         l = 0
         r = 1
         maxProfit = 0
         while(r < len(prices)):
             if(prices[l] > prices[r]):
                 l = r
+                r+=1
             else:
                 profit = prices[r] - prices[l]
-                maxProfit = max(profit, maxProfit)
-            r+=1
+                if(maxProfit < profit):
+                    maxProfit = profit
+                r+=1
         return maxProfit
-        
